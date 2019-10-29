aa8d3ce4e7f5ab4820466c196c8a029c40e53441
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2019-10-29 19:58:55 +0700

    Add files via upload

--- src/tugasPraktikum/jadwalpelajaran.java
+++ src/tugasPraktikum/jadwalpelajaran.java
@@ -13,34 +13,34 @@ public class jadwalpelajaran {
     
          public static void main(String[] args) {
         int choice;
-        Scanner noe = new Scanner (System.in);
+        Scanner p = new Scanner (System.in);
         
         //data guru
-        System.out.println("======Pilihan Pelajaran yang ingin kamu ketahui======");
-        System.out.println("1. Produktif R2");
-        System.out.println("2. Penjaskes R2");
-        System.out.println("3. Sejarah R2");
-        System.out.print("Masukan pilihan");
-        choice = noe.nextInt();
+        System.out.println("===Pilihan Pelajaran yang Tersedia===");
+        System.out.println("1. Produktif RPL");
+        System.out.println("2. Bahasa Indonesia ");
+        System.out.println("3. Matematika  ");
+        System.out.print("Masukan pilihan Anda : ");
+        choice = p.nextInt();
         
         switch (choice){
             case 1 :
                 if(choice == 1);
-                matpel shimme = new matpel ("Muhammad Arifin", "Produktif RPL","setiap hari");
+                matpel shimme = new matpel ("Pak Arifin", "Produktif RPL","senin,selasa,rabu,kamis,jumat");
                 shimme.info();
             break;
             case 2 :
                 if(choice == 2);
-                matpel shimmee = new matpel ("Rofiqut Thoriq", "Pendidikan Jasmani dan Kesehatan","Rabu");
+                matpel shimmee = new matpel ("Pak Tepeng", "Bahasa Indonesia","Senin");
                 shimmee.info();
             break;
             case 3 :
                 if(choice == 3);
-                matpel shimmeee = new matpel ("Ana Wahyuning", "Sejarah","Kamis");
+                matpel shimmeee = new matpel ("Pak Fuad", "Matematika","Senin");
                 shimmeee.info();
             break;
             default:
-                System.out.println("maaf, pilihan tidak tersedia");
+                System.out.println("maaf, pilihan Anda illegal");
                    
                
                     
